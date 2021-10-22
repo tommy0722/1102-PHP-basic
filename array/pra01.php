accdd6486f4c3271ca499887f982a339573665d6
mackliu <macklun@gmail.com>
2021-10-22 11:55:24 +0800

    完成設計陣列練習

--- array/pra01.php
+++ array/pra01.php
@@ -15,21 +15,30 @@
 <body>
     <h1> 建立陣列練習 </h1>
 <?php
-$name=[];
-$subject=['國文','英文','數學','地理','歷史'];
 $name['judy']=['國文'=>95,'英文'=>64,'數學'=>70,'地理'=>90,'歷史'=>84];
-$name['amo']=[95,64,70,90,84];
-$name['john']=[95,64,70,90,84];
-$name['peter']=[95,64,70,90,84];
-$name['hebe']=[95,64,70,90,84];
+$name['amo']=['國文'=>95,'英文'=>64,'數學'=>70,'地理'=>90,'歷史'=>84];
+$name['john']=['國文'=>95,'英文'=>64,'數學'=>70,'地理'=>90,'歷史'=>84];
+$name['peter']=['國文'=>95,'英文'=>64,'數學'=>70,'地理'=>90,'歷史'=>84];
+$name['hebe']=['國文'=>95,'英文'=>64,'數學'=>70,'地理'=>90,'歷史'=>84];
 
 echo "<pre>";
 print_r($name);
-print_r($subject);
 echo "</pre>";
 
- print_r($name['judy']);
+print_r($name['judy']);
 
+$students=array_keys($name);
+
+echo "<pre>";
+print_r($students);
+
+echo "</pre>";
+
+$subject=array_keys($name['judy']);
+echo "<pre>";
+print_r($subject);
+
+echo "</pre>";
  echo "judy的數學成績為".$name['judy']['數學']."分"
 ?>    
 </body>
