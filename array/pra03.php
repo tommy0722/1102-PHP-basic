f25321d7f0b057ad897bd4dcdb5b6e3b96dbe8c8
mackliu <macklun@gmail.com>
2021-10-22 14:48:30 +0800

    列出號碼並美化

--- array/pra03.php
+++ array/pra03.php
@@ -20,10 +20,16 @@
         $i++;
     }
 
-    echo "<pre>";
-    print_r($nums);
-    echo "</pre>";
+    //echo "<pre>";
+    //print_r($nums);
+    //echo "</pre>";
     echo "迴圈次數:".$i;
+    echo "<hr>";
+    foreach($nums as $num){
+        echo "<div class='ball'>".$num ."</div>";
+    }
+    echo "<br>";
+    echo "<div class='ball'>".rand(1,8)."</div>";
 ?>
 </body>
 </html>
