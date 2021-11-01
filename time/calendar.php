5fb7cc79eac3fad03c58d7c134521c2918005827
mackliu <macklun@gmail.com>
2021-11-01 09:49:10 +0800

    修改月曆各周算法,完成當月月曆

--- time/calendar.php
+++ time/calendar.php
@@ -22,6 +22,31 @@
 <body>
     <h1>線上月曆</h1>
     <?php
+
+/*     $firstDay=date("Y-m-01");
+    $month=date("m"); */
+    $firstDay=date("Y-m-01");
+    $month=date("m");
+    $firstWeekWhiteDays=date("w",strtotime($firstDay));
+    $monthDays=date("t",strtotime($firstDay));
+    $firstWeekDays=7-$firstWeekWhiteDays;
+    $weeks=ceil(($firstWeekWhiteDays+$monthDays)/7);
+    $lastWeekDays=($firstWeekWhiteDays+$monthDays)%7;
+    $lastWeekWhiteDays=7-$lastWeekDays;
+    echo "最後一天天數".$lastWeekDays;
+    echo "<br>";
+    echo "weeks".$weeks;
+    echo "<br>";
+        echo "第一周的天數".$firstWeekDays;
+        echo "<br>";
+        echo "monthDays".$monthDays;
+        echo "<br>";
+    echo "firstday".$firstDay;
+    echo "<br>";
+    echo "month".$month;
+    echo "<br>";
+    echo "firstWeekWhiteDays".$firstWeekWhiteDays;
+    echo "<br>";
     echo "<table>";
     echo "<tr>";
     echo "<td>日</td>";
@@ -33,22 +58,46 @@
     echo "<td>六</td>";
     
     echo "</tr>";
-    for($i=0;$i<6;$i++){
+    for($i=0;$i<$weeks;$i++){ //決定當月有幾周
         echo "<tr>";
-        for($j=0;$j<7;$j++){
-            if(($i==0 && $j <5) || ($i==5 && $j>0)){
+        if($i==0){
+            //印空白日
+            for($k=0;$k<$firstWeekWhiteDays;$k++){
+
                 echo "<td>";
                 echo "&nbsp;";
                 echo "</td>";
-
-            }else{
-
+            }
+            
+            for($l=0;$l<$firstWeekDays;$l++){
+                
                 echo "<td>";
-                echo $i*7+$j-4;
+                echo $i*7+$l+1;
+                echo "</td>";
+            }
+            
+        }elseif($i==$weeks-1){
+            for($m=0;$m<$lastWeekDays;$m++){
+                
+                echo "<td>";
+                echo $i*7+$m+1-$firstWeekWhiteDays;
+                echo "</td>";
+            }
+            for($n=0;$n<$lastWeekWhiteDays;$n++){
+                echo "<td>";
+                echo "&nbsp;";
                 echo "</td>";
             }
 
+        }else{
 
+            for($j=0;$j<7;$j++){
+                
+                echo "<td>";
+                echo $i*7+$j+1-$firstWeekWhiteDays;
+                echo "</td>";
+    
+            }
         }
         echo "</tr>";   
     }
